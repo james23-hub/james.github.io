650504db19d2e1de78269389d1fd97b582d470f6
JAMES G. ALVEZ <89216481+james23-hub@users.noreply.github.com>
2022-05-11 23:17:44 +0800

    Create day.js

---
+++ day.js
@@ -0,0 +1,36 @@
+//factory function
+//let x = {value:10};
+//let y = x;
+
+//x.value = 20;
+// let object
+//let obj = {value: 10};
+
+//function increase(obj){
+//	obj.value++;
+//}
+//increase(obj);
+//console.log(obj);
+
+//function add(num1, num2) {
+//return num1 + num2;
+//}
+//function subtract(num1, num2) {
+//return num1 - num2;
+//}
+//function myFunction(num1, num2) {
+//return num1 * num2;
+// myFunction(7,5);
+//}
+//function myFunction(num1, num2) {
+//return num1 / num2;
+// myFunction(3,5);
+//}
+//console.log
+
+const fname = "John";
+
+const year = "Copy Righted 2022";
+
+
+console.log
